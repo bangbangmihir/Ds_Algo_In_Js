83225672de5e60e2c44ffbba121f2c4983dfa9c8
Mihir <mihirsharma60@gmail.com>
2023-07-30 23:00:47 +0530

    Linkedlist Added

--- linkedlist/singly linkedlist/SinglyLinkedList.js
+++ linkedlist/singly linkedlist/SinglyLinkedList.js
@@ -22,3 +22,98 @@
 //Insertion and deletion can be expensive
 //Can quickly be Accesed at a specific index
 
+
+//Push :- Adding Item To linkedlist
+//Popping :- Removeing a node from the linked list 
+
+
+
+
+
+// PseudoCode for Pop
+// 1) If there are no nodes in the list,return undefined
+// 2) Loop through the list until you reach the tail
+// 3) Set the next property of the 2nd last node to be null
+// 4) Set the tail to be 2nd to last node
+// 5) Decrement the length of the list by 1
+// 6) Return the value of the node removed
+
+
+
+
+
+
+
+
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null
+    }
+}
+
+
+class SinglyLinkedList{
+    constructor(){
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+    //Adding push methods
+
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+
+        }
+        this.length ++;
+        return this;
+
+    }
+
+
+    pop(){
+        if(!this.head){
+            return undefined
+        }else{
+            let current = this.head;
+            let newTail = current
+            
+        }
+
+    }
+
+
+
+
+    traverse() {
+        let current = this.head;
+        while(current){
+            console.log(current.val);
+            current = current.next
+        }
+         
+    }
+
+
+}
+
+var list = new SinglyLinkedList();
+list.push("Hi")
+list.push("Mihir")
+list.push("Ravi")
+
+console.log(list)
+console.log(list.traverse())
+
+
+
+
+
+
