e17d4a7a1a4da5630c1ad333a9766e45c49b0dce
Mihir <mihirsharma60@gmail.com>
2025-09-09 09:15:32 +0530

    Two Sum problem Added

---
+++ restart/Array/Two_sum.js
@@ -0,0 +1,41 @@
+// 1. Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Approach 1 : Brute Force
+
+function targetSum(arr, target) {
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    return [];
+
+}
+
+
+console.log(targetSum([2, 7, 11, 15], 9));
+
+
+// Approach 2 : Using HashMap
+
+
+// Time : 25:30
+
+
+
+
+
+
+
+
